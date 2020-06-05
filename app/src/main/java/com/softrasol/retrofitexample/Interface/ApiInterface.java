3e3ba339098fa67996a08c8ad24c1d362e0d73dc
Squadtech <wwwpink44@gmail.com>
2020-06-05 12:23:53 +0500

    Query to get specific data

--- app/src/main/java/com/softrasol/retrofitexample/Interface/ApiInterface.java
+++ app/src/main/java/com/softrasol/retrofitexample/Interface/ApiInterface.java
@@ -6,10 +6,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
 
     @GET("/photos")
     Call<List<UserDataModel>> getEmployees();
+
+    @GET("/photos")
+    Call<List<UserDataModel>> getSpecificEmployee(@Query("employee_id") String id);
 
 }
